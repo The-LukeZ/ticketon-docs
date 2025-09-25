59e417d74b90060e3ecbbfd906b193d45d9b9dc5
Luca Hentschel <luca.hentschel@2peaches.de>
2025-09-25 21:34:55 +0200

    fix: remove starlightThemeNova integration and replace with catppuccin configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import starlightThemeNova from "starlight-theme-nova";
+import catppuccin from "@catppuccin/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
       plugins: [
-        starlightThemeNova({
-          nav: [{ href: "/", label: "Home" }],
+        catppuccin({
+          dark: { accent: "peach", flavor: "mocha" },
+          light: { accent: "peach", flavor: "latte" },
         }),
       ],
       title: "Ticketon",
@@ -43,9 +44,7 @@ export default defineConfig({
         baseUrl: "https://github.com/The-LukeZ/ticketon-docs/tree/main",
       },
       lastUpdated: true,
-      customCss: [
-        "./src/styles/custom.css",
-      ]
+      customCss: ["./src/styles/custom.css"],
     }),
   ],
 });
