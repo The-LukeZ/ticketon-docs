d92397671bb01a96992681de6be4e9c8bcd7d9d6
Luca Hentschel <luca.hentschel@2peaches.de>
2025-09-27 23:52:57 +0200

    feat: configure search options for improved user experience

--- src/config/docsearch.ts
+++ src/config/docsearch.ts
@@ -4,6 +4,8 @@ export default {
   appId: import.meta.env.ALGOLIA_APP_ID,
   apiKey: import.meta.env.ALGOLIA_API_KEY,
   indexName: import.meta.env.ALGOLIA_INDEX_NAME,
+  maxResultsPerGroup: 25,
+  disableUserPersonalization: true,
   getMissingResultsUrl({ query }) {
     return `https://github.com/The-LukeZ/ticketon-docs/issues/new?title=${query}`;
   },
