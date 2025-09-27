5ee0b11a66b0dc7c7dbe171fd00f2bd077a7bff3
Luca Hentschel <luca.hentschel@2peaches.de>
2025-09-28 00:14:17 +0200

    fix: correct indexName in Algolia configuration for consistency

--- src/config/docsearch.ts
+++ src/config/docsearch.ts
@@ -3,7 +3,7 @@ import type { DocSearchClientOptions } from "@astrojs/starlight-docsearch";
 export default {
   appId: "9SJHRRNA4A",
   apiKey: "f210e804b71e0790940955c62fbab1ac",
-  indexName: "ticketon_docs",
+  indexName: "Ticketon Docs v1",
   maxResultsPerGroup: 25,
   disableUserPersonalization: true,
   getMissingResultsUrl({ query }) {
